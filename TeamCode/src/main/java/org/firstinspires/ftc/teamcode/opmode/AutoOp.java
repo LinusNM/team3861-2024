014c405a508e885db202fd533a46026e58ba134d
FTCvashon <bates.atticus@vashonsd.org>
2024-10-12 14:30:24 -0700

    (wip) copy-pasted some code from the april tag sample from git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutoOp.java
@@ -3,9 +3,28 @@ package org.firstinspires.ftc.teamcode.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
 @Disabled
 @Autonomous(name="auto", group="opmode")
 public class AutoOp extends LinearOpMode {
+    private AprilTagProcessor aprilTag;
+
+    private VisionPortal visionPortal;
+
+    public float xLocation;
+    public float yLocation;
+    public float rotation;
+
+
+
     public void runOpMode() {
         //        If it has a consistent starting position then we can start the class with 3 lines
 //        defining the starting position, if not then maybe it’ll have the same starting rotation?
@@ -15,16 +34,77 @@ public class AutoOp extends LinearOpMode {
 ////        (probably separate if/then statements for each variable to differentiate which ones busted)
 ////        (also maybe do the same thing if the reported x or y value is one that would position it outside of the arena? Or if 0 < rotation < 360)
 ////
+        initAprilTag();
+        while (opModeIsActive()) {
+            telemetryAprilTag();
 
-        while (opModeIsActive()){
+            // Push telemetry to the Driver Station.
+            telemetry.update();
+
+            // Save CPU resources; can resume streaming when needed.
+            if (gamepad1.dpad_down) {
+                visionPortal.stopStreaming();
+                telemetry.addLine("dpad down");
+            } else if (gamepad1.dpad_up) {
+                visionPortal.resumeStreaming();
+                telemetry.addLine("dpad up");
+            }
+
+            // if see april tag
+            //    update location
+
 //            Function: probably multiple if/then forever loops that define the x/y coord range and domain
 //        of different areas of the arena?
 //        Ex. if whatever < x < whatever and whatever < y <whatever in_center = true
 //        So like if carrying_brick == true goTo(teamZone)
+/* public void runOpMode()
+    x = whatever
+    y= whatever
+    rotation = 0? i guess?
 
+
+
+ *
+ */
         }
 
 
+    }
+    private void initAprilTag() {
+
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+
+
+
+    }
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
     }
 }
 
