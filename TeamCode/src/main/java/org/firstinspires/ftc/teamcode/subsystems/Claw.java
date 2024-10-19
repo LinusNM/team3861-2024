f2d19bb2d896fa0d6f867763f35fe4662342760e
BarrelAcorn <114117379+BarrelAcorn@users.noreply.github.com>
2024-10-19 11:16:00 -0700

    ContServo power set to stop
    Changed power so the continuous servo will stop at set value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.ServoController;
 
 @TeleOp(name="Claw Control")
 //@Disabled
@@ -24,29 +25,35 @@ public class Claw extends LinearOpMode {
 
         // Set the initial position
         myServoX.setPosition(0.5); // 0.5 is typically the center position
-        myServoY.setPower(0.5);
+        myServoY.setPower(0.367);
         myServoZ.setPosition(0.5);
 
         waitForStart();
+        double change = 0.000001;
+        double power = 0.367;
 
         while (opModeIsActive()) {
 
             // x servo left - right
-            if (gamepad2.right_stick_x > 1) {
+            if (gamepad2.a) {
                 myServoX.setPosition(1); // Move to one extreme
-            } else if (gamepad2.right_stick_x < 1) {
+            } else if (gamepad2.b) {
                 myServoX.setPosition(0); // Move to the other extreme
             } else {
                 myServoX.setPosition(0.5); // Return to center
             }
 
             // y servo up - down
-            if (gamepad2.right_stick_y > 1) {
-                myServoY.setPower(0.7); // Move to one extreme
-            } else if (gamepad2.right_stick_y < 1) {
-                myServoY.setPower(0.3); // Move to the other extreme
-            } else {
-                myServoY.setPower(0.5); // Return to center
+            if (gamepad2.x) {
+                //myServoY.setPower(1); // Move to one extreme
+                power += change;
+            } else if (gamepad2.y) {
+                //myServoY.setPower(0); // Move to the other extreme
+                power -= change;
+            }
+            else {
+                myServoY.setPower(power); // Return to center
+
             }
 
             // z servo open - close
@@ -61,6 +68,7 @@ public class Claw extends LinearOpMode {
             telemetry.addData("Servo Position X", myServoX.getPosition());
             telemetry.addData("Servo Position Y", myServoY.getPower());
             telemetry.addData("Servo Position Z", myServoZ.getPosition());
+            telemetry.addData("Servo power Y", power);
             telemetry.update();
         }
     }
